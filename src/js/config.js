e6595aa49a39496d4cef82b52a82af7b1a26b8bd
Davoooooooo <sargsyan@outlook.com>
2021-10-02 21:31:40 +0400

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 15;
+export const RES_PER_PAGE = 10;
+export const KEY = '7ee81f38-a44b-4e79-9b59-e2ac887fbb66';
+export const MODAL_CLOSE_SEC = 2.5;
